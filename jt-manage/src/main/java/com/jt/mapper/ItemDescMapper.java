33b1d135f40672f3a4333106a0e51a253ff068e6
Administrator <Administrator@tarena>
2019-11-02 16:39:35 +0800

    jt-manage

---
+++ jt-manage/src/main/java/com/jt/mapper/ItemDescMapper.java
@@ -0,0 +1,16 @@
+package com.jt.mapper;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jt.pojo.Item;
+import com.jt.pojo.ItemDesc;
+
+public interface ItemDescMapper extends BaseMapper<ItemDesc>{
+
+
+}
